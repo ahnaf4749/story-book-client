c608668496016181cc93484d4da1e5848435243c
ahnaf4749 <ahnafsagor12@gmail.com>
2022-10-27 20:48:07 +0600

    faq added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-
+        <div className='border-2'>
+            <section className="dark:bg-gray-800 dark:text-gray-100">
+                <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+                    <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
+                    <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
+                        <details>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline"> what is cors?</summary>
+                            <div className="px-4 pb-4">
+                                <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that .</p>
+                            </div>
+                        </details>
+                        <details>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">Why are you using firebase? What other options do you have to implement authentication?</summary>
+                            <div className="px-4 pb-4">
+                                <p> Google Analytics for Firebase allows you to track your users' journey through realtime and custom reporting. As mentioned previously, Firebase supports IOS, Android, Web, and Unity products, allowing you to track your users across a wide range of devices. Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.</p>
+                            </div>
+                        </details>
+                        <details>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">How does the private route work?</summary>
+                            <div className="px-4 pb-4 space-y-2">
+                                <p> Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route (read: page). So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case being authenticated). Authorization goes beyond authentication though. For example, a user can also have roles and permissions wh</p>
+                            </div>
+                        </details>
+                        <details>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">What is Node? How does Node work?</summary>
+                            <div className="px-4 pb-4 space-y-2">
+                                <p> Node. js is an open-source, cross-platform JavaScript runtime environment and library for running web applications outside the client's browser. Ryan Dahl developed it in 2009, and its latest iteration, version 15.14, was released in April 2021. Developers use Node. Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node can, therefore, be used to write server-side applications with access to the operating system, file sys
+                                </p>
+                            </div>
+                        </details>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
